b92fad12134263f528c63571f6da22d0b8296d2b
Hubson404 <60826441+Hubson404@users.noreply.github.com>
2021-01-01 21:48:25 +0100

    CRA-3: Added configuration fields in application.properties

--- src/main/java/org/hubson404/carrentalapp/CarRentalAppApplication.java
+++ src/main/java/org/hubson404/carrentalapp/CarRentalAppApplication.java
@@ -1,11 +1,14 @@
 package org.hubson404.carrentalapp;
 
+import org.hubson404.carrentalapp.domain.CarRentalCompany;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(CarRentalCompany.class)
 @SpringBootApplication (exclude = {SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class, SecurityFilterAutoConfiguration.class})
 public class CarRentalAppApplication {
 
